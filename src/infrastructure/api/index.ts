d04291a41faa896492f17263bfc8f4c15eaf80f3
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-12-27 00:04:35 +0100

    feat: initialize authentication service with Express and TypeScript

---
+++ src/infrastructure/api/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import userRouter from './routes/user';
+import healthRouter from './routes/health';
+
+const app = express();
+app.use(express.json());
+
+app.use("/", (_, res) => {
+  res.send("Welcome to the API");
+});
+
+app.use("/user", userRouter);
+app.use("/health", healthRouter);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from './infrastructure/api/index.js';
+
+const port = 3000;
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
