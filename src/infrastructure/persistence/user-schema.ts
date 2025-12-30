f95073bf989fcd37effdf56b71e14beba2c18a31
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-12-30 21:20:23 +0100

    create db user schema

---
+++ src/infrastructure/persistence/user-schema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  name: varchar({ length: 255 }).notNull(),
+  password: varchar({ length: 255 }).notNull(),
+  salt: varchar({ length: 255 }).notNull(),
+});
+
+export default usersTable;
