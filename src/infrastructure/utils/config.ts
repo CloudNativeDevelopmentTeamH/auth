fee7bc490eb4f4a9aa281bf5ae3d2ca93f767e6b
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-12-30 19:36:47 +0100

    predefine config vars based on .env

--- src/infrastructure/utils/config.ts
+++ src/infrastructure/utils/config.ts
@@ -1,5 +1,13 @@
 import 'dotenv/config';
 
 export default {
-  port: process.env.PORT
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    databaseName: process.env.DB_NAME
+  }
 };
