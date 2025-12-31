724aadd1b982836cfbb292aec7cbd80184c66253
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2025-12-31 02:09:18 +0100

    implement getById for user repository

--- src/infrastructure/persistence/postgres-user-repository.ts
+++ src/infrastructure/persistence/postgres-user-repository.ts
@@ -3,9 +3,26 @@ import type AuthUser from "../../entities/auth-user";
 import type User from "../../entities/user";
 import type UserRepository from "../../usecases/ports/user-repository";
 
+import db from "./drizzle-client";
+import usersTable from "./user-schema";
+import { eq } from "drizzle-orm";
+
 export default class PostgresUserRepository implements UserRepository {
-    async getById(id: number): Promise<User> {
-        return Promise.resolve({} as User);
+    async getById(id: number): Promise<User | null> {
+        const rows = await db
+            .select({
+                id: usersTable.id,
+                name: usersTable.name,
+                email: usersTable.email,
+            })
+            .from(usersTable)
+            .where(eq(usersTable.id, id));
+
+        if (!rows[0]) {
+            return null
+        }
+
+        return rows[0] as User;
     }
     
     async findByEmail(email: string): Promise<AuthUser | null> {
--- src/usecases/ports/user-repository.ts
+++ src/usecases/ports/user-repository.ts
@@ -3,7 +3,7 @@ import type AuthUser from "../../entities/auth-user";
 import User from "../../entities/user";
 
 export default interface UserRepository {
-    getById(id: number): Promise<User>; // logged in, user must exist
+    getById(id: number): Promise<User | null>; // logged in, user must exist
     findByEmail(email: string): Promise<AuthUser | null>; // for login, may not exist
     save(user: NewAuthUser): Promise<User>;
     deleteById(id: number): Promise<boolean>;
