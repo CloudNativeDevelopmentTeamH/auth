2319e1148171f94ea4e76a71984b85a936ca8e65
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2026-01-05 20:14:36 +0100

    cleanup

--- src/infrastructure/utils/services.ts
+++
@@ -1,9 +0,0 @@
-import ArgonPasswordCrypto from "../auth/argon-password-crypto";
-import JwtTokenService from "../auth/jwt-token-service";
-import PostgresUserRepository from "../persistence/postgres-user-repository";
-
-export default {
-  user: PostgresUserRepository,
-  token: JwtTokenService,
-  crypto: ArgonPasswordCrypto
-}
