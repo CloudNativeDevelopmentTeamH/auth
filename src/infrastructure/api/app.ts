0aa16a770adb1c9ecd2cf16b510d4c13c79e8935
Steven Burger <46164156+cwsyth@users.noreply.github.com>
2026-01-26 20:25:08 +0100

    baited

--- src/infrastructure/api/app.ts
+++ src/infrastructure/api/app.ts
@@ -19,10 +19,6 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204
 }));
 
 app.use(middleware.logger);
